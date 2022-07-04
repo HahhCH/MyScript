ea529ca4c3b2a967f90a7632930e657e57857782
HahhCH <57538066+HahhCH@users.noreply.github.com>
2022-07-04 12:29:16 +0800

    Update something.js

--- something.js
+++ something.js
@@ -1,4 +1,4 @@
 var body = $response.body
-    .replace(/\"status\":0/, "\"status\":1")
-    .replace(/\"desc\":\"未认证\"/, "\"desc\":\"已认证\"")
+    .replace(/\"status\":0/,"\"status\":1")
+    .replace(/\"desc\":\"未认证\"/,"\"desc\":\"已认证\"")
 $done({ body });
